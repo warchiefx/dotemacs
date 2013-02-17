3fb628919b62ee6dea112654566bf71b7fdd2d76
Ricardo Restituyo <ricardo@codemera.com>
2013-02-17 12:18:50 -0400

    Added melpa repo

--- site-wcx/wcx-package.el
+++ site-wcx/wcx-package.el
@@ -2,6 +2,8 @@
 (require 'package)
 (package-initialize)
 
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("melpa" . "http://melpa.milkbox.net/packages/")))
 
 (provide 'wcx-package)
