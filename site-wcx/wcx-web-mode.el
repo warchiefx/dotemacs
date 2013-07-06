3302609c199dadcba22a6d399b82ac956088869d
Ricardo Restituyo <ricardo@codemera.com>
2013-07-06 01:40:13 -0400

    Added web-mode instead of nxhtml

---
+++ site-wcx/wcx-web-mode.el
@@ -0,0 +1,21 @@
+
+;;; Code:
+
+(when (locate-library "web-mode")
+  (load-library "web-mode")
+
+  (add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsp\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+  (setq web-mode-engines-alist
+        '(("django"    . "\\.html\\'")
+        )))
+
+(provide 'wcx-web-mode)
+;;; wcx-web-mode.el ends here
