db70727df61430b12311a3d998053377d67c00dc
Ricardo Restituyo <ricardo@codemera.com>
2013-07-14 15:13:35 -0400

    Customized helm display

--- site-wcx/wcx-helm.el
+++ site-wcx/wcx-helm.el
@@ -1,6 +1,29 @@
-
 (when (locate-library "helm")
   (helm-mode 1)
-  (global-set-key [C-f2] 'helm-mini))
+
+  (when (locate-library "helm-c-moccur")
+    (load-library "helm-c-moccur"))
+  (when (locate-library "helm-projectile")
+    (load-library "helm-projectile"))
+
+  (defun wcx-helm ()
+    (interactive)
+    (let ((default (thing-at-point 'symbol)))
+      (helm
+       :prompt "pattern: "
+       :sources
+       (append '(helm-source-buffers-list
+                 (when (projectile-project-p)
+                   helm-c-source-projectile-files-list)
+                 helm-c-source-browse-code
+                 helm-c-source-occur-by-moccur
+                 helm-source-recentf
+                 helm-source-files-in-current-dir
+                 helm-source-pp-bookmarks
+                 helm-source-buffer-not-found
+                 )
+               ))))
+
+  (global-set-key [C-f2] 'wcx-helm))
 
 (provide 'wcx-helm)
