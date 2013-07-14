39ce7fd2178d39fdb8135782754c1994635e490d
Ricardo Restituyo <ricardo@codemera.com>
2013-07-14 12:08:15 -0400

    Added Projectile support

---
+++ site-wcx/wcx-projects.el
@@ -0,0 +1,11 @@
+
+(when (locate-library "projectile")
+  (load-library "projectile")
+  (projectile-global-mode))
+
+(when (locate-library "helm-projectile")
+  (load-library "helm-projectile"))
+
+
+(provide 'wcx-projects)
+;;; wcx-projects.el ends here
