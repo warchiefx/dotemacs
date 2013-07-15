d3048c2f0bcf7a3c27adf86a1112a3db18fa39fa
Ricardo Restituyo <ricardo@codemera.com>
2013-07-14 23:55:28 -0400

    Improved magit-status

--- site-wcx/wcx-git.el
+++ site-wcx/wcx-git.el
@@ -22,4 +22,19 @@
   (require 'magit)
   (global-set-key [?\C-c ?\C-x ?g] 'magit-status))
 
+;; full screen magit-status
+
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
 (provide 'wcx-git)
