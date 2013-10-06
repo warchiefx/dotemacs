c6ab43466d96c7b177d8ad7bb244c5c66391b0e9
Ricardo Restituyo <ricardo@codemera.com>
2013-10-06 11:49:35 -0400

    Use latest org version from site-packages instead of builtin or elpa package

--- site-wcx/wcx-org.el
+++ site-wcx/wcx-org.el
@@ -13,7 +13,11 @@
 
 ;; Org-mode setup
 ;; The following lines are always needed. Choose your own keys.
+
+(add-to-list 'load-path (concat site-lisp-path "org-mode/lisp"))
+
 (when (locate-library "org")
+  (load-library "org")
   (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 
   (global-set-key "\C-cl" 'org-store-link)
@@ -63,42 +67,7 @@
                                    ))
   (add-hook 'display-time-hook 'wcx/org-clocking-info-to-file))
 
-
-;; From: http://emacs-fu.blogspot.com/2009/10/writing-presentations-with-org-mode-and.html
-;; allow for export=>beamer by placing
-;; #+LaTeX_CLASS: beamer in org files
-;; (unless (boundp 'org-export-latex-classes)
-;;   (setq org-export-latex-classes nil))
-;; (add-to-list 'org-export-latex-classes
-;;              '("beamer"
-;;                "\\documentclass[11pt]{beamer}\n
-;;       \\mode<presentation>\n
-;;       \\usetheme{Antibes}\n
-;;       \\usecolortheme{lily}\n
-;;       \\beamertemplateballitem\n
-;;       \\setbeameroption{show notes}
-;;       \\usepackage[utf8]{inputenc}\n
-;;       \\usepackage[T1]{fontenc}\n
-;;       \\usepackage{hyperref}\n
-;;       \\usepackage{color}
-;;       \\usepackage{listings}
-;;       \\lstset{numbers=none,language=[ISO]C++,tabsize=4,
-;;   frame=single,
-;;   basicstyle=\\small,
-;;   showspaces=false,showstringspaces=false,
-;;   showtabs=false,
-;;   keywordstyle=\\color{blue}\\bfseries,
-;;   commentstyle=\\color{red},
-;;   }\n
-;;       \\usepackage{verbatim}\n
-;;       \\institute{Miskatonic University, Astrology Dept.}\n
-;;        \\subject{RMRF}\n"
-
-;;                ("\\section{%s}" . "\\section*{%s}")
-
-;;                ("\\begin{frame}[fragile]\\frametitle{%s}"
-;;                 "\\end{frame}"
-;;                 "\\begin{frame}[fragile]\\frametitle{%s}"
-;;                 "\\end{frame}")))
+;; (when (locate-library "ox-reveal")
+;;   (require 'org-reveal))
 
 (provide 'wcx-org)
