30bb36b8fdb85f4db7444610bf6be4bae8f23813
Ricardo Restituyo <ricardo@codemera.com>
2013-10-06 11:48:24 -0400

    Added tern package for JS

--- site-wcx/wcx-javascript.el
+++ site-wcx/wcx-javascript.el
@@ -17,4 +17,15 @@
   (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
   (setq js2-use-font-lock-faces t))
 
+(when (locate-library "tern")
+  (autoload 'tern-mode "tern.el" nil t)
+  (autoload 'tern-auto-complete "tern-auto-complete.el" nil t)
+  
+  (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
+
+  (eval-after-load 'tern
+    '(progn
+       (require 'tern-auto-complete)
+       (tern-ac-setup))))
+
 (provide 'wcx-javascript)
