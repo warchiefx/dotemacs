06293145dae3a5cbf72c5af950d2610f8a5ffedc
Ricardo Restituyo <ricardo@codemera.com>
2013-10-06 11:47:58 -0400

    Added org elpa repo

--- site-wcx/wcx-package.el
+++ site-wcx/wcx-package.el
@@ -4,6 +4,7 @@
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
                          ("marmalade" . "http://marmalade-repo.org/packages/")
-                         ("melpa" . "http://melpa.milkbox.net/packages/")))
+                         ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("org" . "http://orgmode.org/elpa/")))
 
 (provide 'wcx-package)
