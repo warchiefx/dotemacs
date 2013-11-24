aa841615a63c922d1c84f775f43eb522a1aea5ba
Ricardo Restituyo <ricardo@codemera.com>
2013-11-24 16:36:20 -0400

    Cleaned up auto-complete mode setup

--- site-wcx/wcx-auto-complete.el
+++ site-wcx/wcx-auto-complete.el
@@ -1,13 +1,10 @@
 ;; Using auto-complete mode from https://github.com/m2ym/auto-complete
 
-(add-to-list 'load-path (concat site-lisp-path "auto-complete"))
-
-(require 'auto-complete)
-
-(add-to-list 'ac-dictionary-directories "~/.emacs.d/site-lisp/auto-complete/dict")
-(require 'auto-complete-config)
-(ac-config-default)
-
-(global-auto-complete-mode t)
+(when (locate-library "auto-complete")
+  (require 'auto-complete)
+  (require 'auto-complete-config)
+  (ac-config-default)
+  (global-auto-complete-mode t))
 
 (provide 'wcx-auto-complete)
+;;; wcx-auto-complete ends here
