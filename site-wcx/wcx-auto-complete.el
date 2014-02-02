7aec382d4b6217ef08f74c8aeb52a360982f7c1c
Ricardo Restituyo <ricardo@codemera.com>
2014-02-02 16:42:15 -0400

    Add android specific completion settings

--- site-wcx/wcx-auto-complete.el
+++ site-wcx/wcx-auto-complete.el
@@ -4,7 +4,18 @@
   (require 'auto-complete)
   (require 'auto-complete-config)
   (ac-config-default)
-  (global-auto-complete-mode t))
+  (global-auto-complete-mode t)
+
+  ;; android specific settings
+  ;; AndroidManifest.xml
+  (defun ac-android-manifest-nxml-setup()
+    (when (string= (buffer-name) "AndroidManifest.xml")
+      (setq ac-sources '(ac-source-yasnippet
+                         ac-source-abbrev
+                         ac-source-dictionary
+                         ac-source-words-in-same-mode-buffers))
+      ((lambda () (auto-complete-mode 1)))))
+  (add-hook 'nxml-mode-hook 'ac-android-manifest-nxml-setup))
 
 (provide 'wcx-auto-complete)
 ;;; wcx-auto-complete ends here
