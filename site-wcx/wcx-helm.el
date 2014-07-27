6759b665ab25f828960760826b59549837b8f957
Ricardo Restituyo <ricardo@codemera.com>
2014-07-27 17:20:25 -0400

    Extra helm config, remap M-y to use helm-browse-killring

--- site-wcx/wcx-helm.el
+++ site-wcx/wcx-helm.el
@@ -9,6 +9,30 @@
   (helm-mode 0)
   (helm-adaptative-mode)
 
+  (setq
+   helm-google-suggest-use-curl-p t
+   helm-scroll-amount 4 ; scroll 4 lines other window using M-<next>/M-<prior>
+   helm-quick-update t ; do not display invisible candidates
+   helm-idle-delay 0.01 ; be idle for this many seconds, before updating in delayed sources.
+   helm-input-idle-delay 0.01 ; be idle for this many seconds, before updating candidate buffer
+   helm-ff-search-library-in-sexp t ; search for library in `require' and `declare-function' sexp.
+
+   helm-split-window-default-side 'other ;; open helm buffer in another window
+   helm-split-window-in-side-p t ;; open helm buffer inside current window, not occupy whole other window
+   helm-buffers-favorite-modes (append helm-buffers-favorite-modes
+                                       '(picture-mode artist-mode))
+   helm-candidate-number-limit 200 ; limit the number of displayed canidates
+   helm-M-x-requires-pattern 0     ; show all candidates when set to 0
+   helm-boring-file-regexp-list
+   '("\\.git$" "\\.hg$" "\\.svn$" "\\.CVS$" "\\._darcs$" "\\.la$" "\\.o$" "\\.i$") ; do not show these files in helm buffer
+   helm-ff-file-name-history-use-recentf t
+   helm-move-to-line-cycle-in-source t ; move to end or beginning of source
+                                        ; when reaching top or bottom of source.
+   ido-use-virtual-buffers t      ; Needed in helm-buffers-list
+   helm-buffers-fuzzy-matching t          ; fuzzy matching buffer names when non--nil
+                                        ; useful in helm-mini that lists buffers
+   )
+
   (when (locate-library "helm-c-moccur")
     (load-library "helm-c-moccur"))
   (when (locate-library "helm-projectile")
@@ -33,6 +57,7 @@
           :prompt "> "))
 
   (global-set-key [f1] 'wcx-helm)
-  (global-set-key [S-f1] 'helm-do-grep))
+  (global-set-key [S-f1] 'helm-do-grep)
+  (global-set-key (kbd "M-y") 'helm-show-kill-ring))
 
 (provide 'wcx-helm)
