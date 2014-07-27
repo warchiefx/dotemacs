75ab1c2a0a966a4e4b4edc4db4a40bbc960cc051
Ricardo Restituyo <ricardo@codemera.com>
2014-07-27 00:47:36 -0400

    Insert _ instead of space if defining python functions

--- site-wcx/wcx-python.el
+++ site-wcx/wcx-python.el
@@ -8,5 +8,22 @@
 
 (setq python-saved-check-command nil)
 
+
+;; Taken and adapted from: http://railscoder.net/emacs/elisp/ruby/2014/07/16/emacs-ruby-space-replace/
+(defun kdt-python-method-space-replace ()
+  "When pressing space while naming a defined method, insert an underscore"
+  (interactive)
+  (if (and (looking-back "def .+")
+           (not (and
+                 (looking-at ".*)$")
+                 (looking-back "(.*"))))
+      (insert "_")
+    (insert " ")))
+
+(eval-after-load 'elpy-mode
+  '(progn
+     (define-key elpy-mode-map (kbd "SPC") 'kdt-python-method-space-replace)))
+
+
 (provide 'wcx-python)
 ;;; wcx-python.el ends here
