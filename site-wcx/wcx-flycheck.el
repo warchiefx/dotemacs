462fc7715d5fafaf8d0c9e08677be218e01fa0d2
Ricardo Restituyo <ricardo@codemera.com>
2014-07-27 00:08:20 -0400

    Show flycheck warns/errors as tooltips

--- site-wcx/wcx-flycheck.el
+++ site-wcx/wcx-flycheck.el
@@ -1,6 +1,15 @@
+;;; wcx-flycheck.el -- Flycheck config
 
+;;; Code:
 (when (locate-library "flycheck")
   (load-library "flycheck")
-  (add-hook 'after-init-hook #'global-flycheck-mode))
+  (add-hook 'after-init-hook #'global-flycheck-mode)
+
+  (when (locate-library "flycheck-pos-tip")
+    (load-library "flycheck-pos-tip")
+    (eval-after-load 'flycheck
+      '(custom-set-variables
+        '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))))
 
 (provide 'wcx-flycheck)
+;;; wcx-flycheck ends here
