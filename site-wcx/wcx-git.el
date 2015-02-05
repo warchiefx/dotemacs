f1cda05b2cbaf724ca174242dfa51db055c71c16
Ricardo Restituyo <ricardo@codemera.com>
2015-02-05 11:29:29 -0400

    Enable magit-gitflow

--- site-wcx/wcx-git.el
+++ site-wcx/wcx-git.el
@@ -44,4 +44,8 @@
 (when (locate-library "gist")
   (load-library "gist"))
 
+(when (locate-library "magit-gitflow")
+  (require 'magit-gitflow)
+  (add-hook 'magit-mode-hook 'turn-on-magit-gitflow))
+
 (provide 'wcx-git)
