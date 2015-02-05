3f0ee6327fd81d3b6e4faa2bdddb2012ec9ea6cb
Ricardo Restituyo <ricardo@codemera.com>
2015-02-05 11:25:40 -0400

    Disable flycheck-pos-tip

--- site-wcx/wcx-flycheck.el
+++ site-wcx/wcx-flycheck.el
@@ -5,11 +5,12 @@
   (load-library "flycheck")
   (add-hook 'after-init-hook #'global-flycheck-mode)
 
-  (when (locate-library "flycheck-pos-tip")
-    (load-library "flycheck-pos-tip")
-    (eval-after-load 'flycheck
-      '(custom-set-variables
-        '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages)))))
+  ;; (when (locate-library "flycheck-pos-tip")
+  ;;   (load-library "flycheck-pos-tip")
+  ;;   (eval-after-load 'flycheck
+  ;;     '(custom-set-variables
+  ;;       '(flycheck-display-errors-function #'flycheck-pos-tip-error-messages))))
+  )
 
 (provide 'wcx-flycheck)
 ;;; wcx-flycheck ends here
