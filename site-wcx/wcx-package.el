02ca3aa3057029425e7e8e2ebea0ebecb5ade93f
Ricardo Restituyo <ricardo@codemera.com>
2015-11-06 01:00:19 -0400

    Add elpy repo, use https for marmalade

--- site-wcx/wcx-package.el
+++ site-wcx/wcx-package.el
@@ -3,8 +3,9 @@
 (package-initialize)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")
+                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
+                         ("elpy" . "http://jorgenschaefer.github.io/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
 
 (provide 'wcx-package)
