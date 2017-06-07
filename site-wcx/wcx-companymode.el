5d798a996a9ad4b295b53f08f5b2ce14e847d882
Ricardo Restituyo <warchiefx@gmail.com>
2017-06-07 18:48:02 -0400

    Tweak company-mode setup

--- site-wcx/wcx-companymode.el
+++ site-wcx/wcx-companymode.el
@@ -1,7 +1,12 @@
 (use-package company
   :ensure t
   :demand t
-  :diminish company-mode)
+  :diminish company-mode
+  :config
+  (setq-default
+   company-idle-delay .2
+   company-minimum-prefix-length 1
+   company-tooltip-align-annotations t))
 
 (use-package company-quickhelp
   :ensure t
@@ -13,5 +18,12 @@
 
 (global-company-mode 1)
 
+(use-package company-tern
+  :after company
+  :config
+  (add-to-list 'company-backends 'company-tern)
+  (setq-default
+   company-tern-meta-as-single-line t
+   company-tern-property-marker " *"))
 
 (provide 'wcx-companymode)
