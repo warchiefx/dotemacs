b3a2c73d2b4bbaa8d5d06dfb4611ea109651f492
Ricardo Restituyo <warchiefx@gmail.com>
2017-06-07 18:48:29 -0400

    Flycheck tweaks

--- site-wcx/wcx-flycheck.el
+++ site-wcx/wcx-flycheck.el
@@ -5,6 +5,10 @@
 (use-package "flycheck"
   :ensure t
   :config
+  (setq-default
+   flycheck-check-syntax-automatically '(save mode-enabled)
+   flycheck-disabled-checkers '(emacs-lisp-checkdoc)
+   flycheck-display-errors-delay .3)
   (global-flycheck-mode 1)
   :diminish flycheck-mode)
 
