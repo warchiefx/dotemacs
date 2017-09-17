25682419fa24106423db44b9077eede0c08922b8
Ricardo Restituyo <warchiefx@gmail.com>
2017-09-17 18:49:39 -0400

    Add comment-tags mode

--- site-wcx/wcx-generic.el
+++ site-wcx/wcx-generic.el
@@ -303,5 +303,28 @@ or the current buffer directory."
   :config
   (add-hook 'makefile-mode-hook 'makefile-executor-mode))
 
+
+(use-package comment-tags
+  :ensure t
+  :config
+  (autoload 'comment-tags-mode "comment-tags-mode")
+  (setq comment-tags-keymap-prefix (kbd "C-c t"))
+  (with-eval-after-load "comment-tags"
+    (setq comment-tags-keyword-faces
+          `(("TODO" . ,(list :weight 'bold :foreground "#28ABE3"))
+            ("FIXME" . ,(list :weight 'bold :foreground "#DB3340"))
+            ("BUG" . ,(list :weight 'bold :foreground "#DB3340"))
+            ("HACK" . ,(list :weight 'bold :foreground "#E8B71A"))
+            ("KLUDGE" . ,(list :weight 'bold :foreground "#E8B71A"))
+            ("XXX" . ,(list :weight 'bold :foreground "#F7EAC8"))
+            ("INFO" . ,(list :weight 'bold :foreground "#F7EAC8"))
+            ("DONE" . ,(list :weight 'bold :foreground "#1FDA9A"))))
+    (setq comment-tags-comment-start-only t
+          comment-tags-require-colon nil
+          comment-tags-case-sensitive t
+          comment-tags-show-faces t
+          comment-tags-lighter nil))
+  (add-hook 'prog-mode-hook 'comment-tags-mode))
+
 (provide 'wcx-generic)
 ;;; wcx-generic.el ends here
