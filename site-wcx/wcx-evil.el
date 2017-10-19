79b8bc578fc792718d09b2c82b61a8689aa73241
Ricardo Restituyo <rrestituyo@vivial.net>
2017-10-19 09:11:26 -0400

    Add evil-args

--- site-wcx/wcx-evil.el
+++ site-wcx/wcx-evil.el
@@ -119,4 +119,20 @@
     "pa" 'helm-projectile-find-file-in-known-projects
   ))
 
+(use-package evil-args
+  :ensure t
+  :config
+  ;; bind evil-args text objects
+  (define-key evil-inner-text-objects-map "a" 'evil-inner-arg)
+  (define-key evil-outer-text-objects-map "a" 'evil-outer-arg)
+
+  ;; bind evil-forward/backward-args
+  (define-key evil-normal-state-map "L" 'evil-forward-arg)
+  (define-key evil-normal-state-map "H" 'evil-backward-arg)
+  (define-key evil-motion-state-map "L" 'evil-forward-arg)
+  (define-key evil-motion-state-map "H" 'evil-backward-arg)
+
+  ;; bind evil-jump-out-args
+  (define-key evil-normal-state-map "K" 'evil-jump-out-args))
+
 (provide 'wcx-evil)
