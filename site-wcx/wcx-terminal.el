7ccdda8ff42cfab5934414e2aecdb78f771744f6
Ricardo Restituyo <rrestituyo@vivial.net>
2017-12-21 09:31:53 -0400

    Use eshell instead of normal shells

--- site-wcx/wcx-terminal.el
+++ site-wcx/wcx-terminal.el
@@ -13,7 +13,6 @@
 
 (use-package shell-pop
   :ensure t
-  :after sane-term
   :bind (([f7] . shell-pop)
          ([shift f7] . sane-term-create)))
 
@@ -27,5 +26,75 @@
   ;; comint uninstall
   (progn (remove-hook 'comint-preoutput-filter-functions 'xterm-color-filter)
          (add-to-list 'comint-output-filter-functions 'ansi-color-process-output)))
+
+;; Setup eshell smart
+(require 'eshell)
+(require 'em-smart)
+(setq eshell-where-to-jump 'begin)
+(setq eshell-review-quick-commands nil)
+(setq eshell-smart-space-goes-to-end t)
+
+(setq eshell-history-size 1024)
+(setq eshell-prompt-regexp "^[^#$]*[#$] ")
+
+(load "em-hist")           ; So the history vars are defined
+(if (boundp 'eshell-save-history-on-exit)
+    (setq eshell-save-history-on-exit t)) ; Don't ask, just save
+;(message "eshell-ask-to-save-history is %s" eshell-ask-to-save-history)
+(if (boundp 'eshell-ask-to-save-history)
+    (setq eshell-ask-to-save-history 'always)) ; For older(?) version
+;(message "eshell-ask-to-save-history is %s" eshell-ask-to-save-history)
+
+(defun eshell/ef (fname-regexp &rest dir) (ef fname-regexp default-directory))
+
+
+;;; ---- path manipulation
+
+(defun pwd-repl-home (pwd)
+  (interactive)
+  (let* ((home (expand-file-name (getenv "HOME")))
+   (home-len (length home)))
+    (if (and
+   (>= (length pwd) home-len)
+   (equal home (substring pwd 0 home-len)))
+  (concat "~" (substring pwd home-len))
+      pwd)))
+
+(defun curr-dir-git-branch-string (pwd)
+  "Returns current git branch as a string, or the empty string if
+PWD is not in a git repo (or the git command is not found)."
+  (interactive)
+  (when (and (eshell-search-path "git")
+             (locate-dominating-file pwd ".git"))
+    (let ((git-output (shell-command-to-string (concat "cd " pwd " && git branch | grep '\\*' | sed -e 's/^\\* //'"))))
+      (propertize (concat "["
+              (if (> (length git-output) 0)
+                  (substring git-output 0 -1)
+                "(no branch)")
+              "]") 'face `(:foreground "yellow"))
+      )))
+
+(setq eshell-prompt-function
+      (lambda ()
+        (concat
+         (propertize ((lambda (p-lst)
+            (if (> (length p-lst) 3)
+                (concat
+                 (mapconcat (lambda (elm) (if (zerop (length elm)) ""
+                                            (substring elm 0 1)))
+                            (butlast p-lst 3)
+                            "/")
+                 "/"
+                 (mapconcat (lambda (elm) elm)
+                            (last p-lst 3)
+                            "/"))
+              (mapconcat (lambda (elm) elm)
+                         p-lst
+                         "/")))
+          (split-string (pwd-repl-home (eshell/pwd)) "/")) 'face `(:foreground "cyan"))
+         (or (curr-dir-git-branch-string (eshell/pwd)))
+         (propertize "# " 'face 'default))))
+
+(setq eshell-highlight-prompt nil)
 
 (provide 'wcx-terminal)
