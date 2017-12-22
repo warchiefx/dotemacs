cceeb71f060fb49d185310e40ff8c462b8c673b1
Ricardo Restituyo <rrestituyo@vivial.net>
2017-12-22 15:02:20 -0400

    Make evil respect indium

--- site-wcx/wcx-evil.el
+++ site-wcx/wcx-evil.el
@@ -16,6 +16,7 @@
   (evil-set-initial-state 'dashboard-mode 'emacs)
   (evil-set-initial-state 'neotree-mode 'emacs)
   (evil-set-initial-state 'eww-mode 'emacs)
+  (evil-set-initial-state 'indium-repl-mode 'emacs)
   (add-hook 'git-commit-mode-hook 'evil-emacs-state)
   (evil-set-initial-state 'magit-log-edit-mode 'insert)
   (evil-mode 1))
