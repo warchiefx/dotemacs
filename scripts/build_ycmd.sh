2d846efce6c2786150fc2e01d3466330f83fbc05
Ricardo Restituyo <warchiefx@gmail.com>
2018-05-26 15:19:25 -0400

    Add ycmd submodule

---
+++ scripts/build_ycmd.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+EMACS_DIR=~/.emacs.d
+YCMD_DIR=${EMACS_DIR}/ycmd
+
+function checkout_ycmd {
+    cd $EMACS_DIR
+    git submodule update --init --recursive
+}
+
+
+function build_ycmd_module {
+    cd $YCMD_DIR
+    ./build.py --clang-completer --rust-completer --js-completer --java-completer
+}
+
+if [ -d ${YCMD_DIR} -a -z "$(ls -A ${YCMD_DIR})" ]; then
+    checkout_ycmd
+fi
+build_ycmd_module
