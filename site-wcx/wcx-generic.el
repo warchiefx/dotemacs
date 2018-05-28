aa9b4860fa9aac6a892c1d5ba6f6cab3ff989d26
Ricardo Restituyo <warchiefx@gmail.com>
2018-05-28 09:35:30 -0400

    Improved switch-window setup

--- site-wcx/wcx-generic.el
+++ site-wcx/wcx-generic.el
@@ -148,7 +148,21 @@
 
 (use-package switch-window
   :ensure t
-  :bind (("C-x o" . switch-window)))
+  :bind (("C-x o" . switch-window)
+         ("C-x 1" . switch-window-then-maximize)
+         ("C-x 2" . switch-window-then-split-below)
+         ("C-x 3" . switch-window-then-split-right)
+         ("C-x 0" . switch-window-then-delete)
+
+         ("C-x 4 d" . switch-window-then-dired)
+         ("C-x 4 f" . switch-window-then-find-file)
+         ("C-x 4 m" . switch-window-then-compose-mail)
+         ("C-x 4 r" . switch-window-then-find-file-read-only)
+
+         ("C-x 4 C-f" . switch-window-then-find-file)
+         ("C-x 4 C-o" . switch-window-then-display-buffer)
+
+         ("C-x 4 0" . switch-window-then-kill-buffer)))
 
 
 ;; Avoid having to delete extra spaces after kill-line on end of line
