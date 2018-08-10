432ec7a189f191d0a4dec57bfd6a6d0ecc12041b
Ricardo Restituyo <warchiefx@gmail.com>
2018-08-10 17:15:54 -0400

    Remove marmalade-repo

--- site-wcx/wcx-package.el
+++ site-wcx/wcx-package.el
@@ -3,7 +3,6 @@
 ;; (package-initialize)
 
 (setq package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
-                         ("marmalade" . "https://marmalade-repo.org/packages/")
                          ("melpa" . "http://melpa.milkbox.net/packages/")
                          ("elpy" . "http://jorgenschaefer.github.io/packages/")
                          ("org" . "http://orgmode.org/elpa/")))
