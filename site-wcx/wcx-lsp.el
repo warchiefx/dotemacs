6f96307dc99b77fab8b3ab0afefb81fefb51e159
Ricardo Restituyo <warchiefx@gmail.com>
2018-11-19 10:41:17 -0400

    Use eglot-mode for lsp

--- site-wcx/wcx-lsp.el
+++ site-wcx/wcx-lsp.el
@@ -1,58 +1,20 @@
-(use-package lsp-java
-  :ensure t
-  :requires (lsp-ui-flycheck lsp-ui-sideline)
-  :config
-  (add-hook 'java-mode-hook  'lsp-java-enable)
-  (add-hook 'java-mode-hook  'flycheck-mode)
-  (add-hook 'java-mode-hook  'company-mode)
-  (add-hook 'java-mode-hook  (lambda () (lsp-ui-flycheck-enable t)))
-  (add-hook 'java-mode-hook  'lsp-ui-sideline-mode)
-  ;; (setq lsp-java--workspace-folders (list (error "XXX Specify your projects here")))
-  )
-
-(use-package lsp-mode
+(use-package eglot
   :ensure t
   :config
+  (defun wcx/activate-lsp ()
+    (flycheck-mode 0)
+    (eglot-ensure))
 
-  ;; make sure we have lsp-imenu everywhere we have LSP
-  (require 'lsp-imenu)
-  (add-hook 'lsp-after-open-hook 'lsp-enable-imenu)
-  ;; get lsp-python-enable defined
-  ;; NB: use either projectile-project-root or ffip-get-project-root-directory
-  ;;     or any other function that can be used to find the root directory of a project
-  (lsp-define-stdio-client lsp-python "python"
-                           #'projectile-project-root
-                           '("pyls"))
+  (defun wcx/find-projectile-project (dir)
+    (if (fboundp 'projectile-project-root)
+        (let ((root (projectile-project-root dir)))
+          (and root (cons 'transient root)))))
 
-  ;; make sure this is activated when python-mode is activated
-  ;; lsp-python-enable is created by macro above
-  (add-hook 'python-mode-hook
-            (lambda ()
-              (lsp-python-enable)))
+  (add-to-list 'project-find-functions #'wcx/find-projectile-project)
 
-  ;; lsp extras
-  (use-package lsp-ui
-    :ensure t
-    :config
-    (setq lsp-ui-sideline-ignore-duplicate t)
-    (add-hook 'lsp-mode-hook 'lsp-ui-mode))
-
-  (use-package company-lsp
-    :ensure t
-    :config
-    (push 'company-lsp company-backends))
-
-  ;; NB: only required if you prefer flake8 instead of the default
-  ;; send pyls config via lsp-after-initialize-hook -- harmless for
-  ;; other servers due to pyls key, but would prefer only sending this
-  ;; when pyls gets initialised (:initialize function in
-  ;; lsp-define-stdio-client is invoked too early (before server
-  ;; start)) -- cpbotha
-  (defun lsp-set-cfg ()
-    (let ((lsp-cfg `(:pyls (:configurationSources ("flake8")))))
-      ;; TODO: check lsp--cur-workspace here to decide per server / project
-      (lsp--set-configuration lsp-cfg)))
-
-  (add-hook 'lsp-after-initialize-hook 'lsp-set-cfg))
+  (add-hook 'java-mode-hook 'wcx/activate-lsp)
+  (add-hook 'python-mode-hook 'wcx/activate-lsp)
+  (add-hook 'ruby-mode-hook 'wcx/activate-lsp)
+  (add-hook 'bash-mode-hook 'wcx/activate-lsp))
 
 (provide 'wcx-lsp)
