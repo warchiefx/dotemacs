79840ec3d3f43a4855f35b264147def3adea9573
Ricardo Restituyo <warchiefx@gmail.com>
2019-03-04 15:20:59 -0400

    Add icons to file selectors

--- site-wcx/wcx-modeline.el
+++ site-wcx/wcx-modeline.el
@@ -44,6 +44,15 @@
 (use-package all-the-icons
   :ensure t)
 
+(use-package all-the-icons-ivy
+  :ensure t
+  :after (all-the-icons ivy)
+  :custom (all-the-icons-ivy-buffer-commands '(ivy-switch-buffer-other-window))
+  :config
+  (add-to-list 'all-the-icons-ivy-file-commands 'counsel-dired-jump)
+  (add-to-list 'all-the-icons-ivy-file-commands 'counsel-find-library)
+  (all-the-icons-ivy-setup))
+
 ;; (defun format-icon (func icon-name color)
 ;;   (let ((icon (funcall func icon-name)))
 ;;     (propertize icon
