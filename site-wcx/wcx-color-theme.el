1add1807f1e4ea6889fdcec33b3892012552d558
Ricardo Restituyo <warchiefx@gmail.com>
2019-07-19 14:26:14 -0400

    Add solaire-mode

--- site-wcx/wcx-color-theme.el
+++ site-wcx/wcx-color-theme.el
@@ -113,6 +113,14 @@
                 (set-face-attribute atom nil
                                     :underline underline)))))
 
+(use-package solaire-mode
+  :hook
+  ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
+  (minibuffer-setup . solaire-mode-in-minibuffer)
+  :config
+  (solaire-global-mode +1)
+  (solaire-mode-swap-bg))
+
 (provide 'wcx-color-theme)
 
 ;;; wcx-color-theme.el ends here
