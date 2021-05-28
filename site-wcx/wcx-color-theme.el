ad708e3461c42d9763611c1d76c103d4e0b4046b
Ricardo Restituyo <warchiefx@gmail.com>
2021-05-28 09:44:01 -0400

    Deactivate solaire mode

--- site-wcx/wcx-color-theme.el
+++ site-wcx/wcx-color-theme.el
@@ -137,13 +137,13 @@
                 (set-face-attribute atom nil
                                     :underline underline)))))
 
-(use-package solaire-mode
-  :hook
-  ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
-  (minibuffer-setup . solaire-mode-in-minibuffer)
-  :config
-  (solaire-global-mode +1)
-  (solaire-mode-swap-bg))
+;;(use-package solaire-mode
+;; :hook
+;;  ((change-major-mode after-revert ediff-prepare-buffer) . turn-on-solaire-mode)
+;;  (minibuffer-setup . solaire-mode-in-minibuffer)
+;;   :config
+;;  (solaire-global-mode +1)
+;;  (solaire-mode-swap-bg))
 
 (provide 'wcx-color-theme)
 
